01044e12164d4a1dfff931706806e8bd145a205c
William Anderson <53445030+andersonw1@users.noreply.github.com>
2024-06-21 08:07:40 -0700

    Update and rename train_bg1d_simdata.sh to bg1d_testdata.sh

--- BurgersEqn1D_multiplesamples/bg1d_testdata.sh
+++ BurgersEqn1D_multiplesamples/bg1d_testdata.sh
@@ -2,10 +2,10 @@
 #SBATCH -N 1
 #SBATCH -t 0:20:00
 
-chmod +x generate_data_sim.py
+chmod +x generate_data_test.py
 
 #source  /usr/workspace/$USER/local/$SYS_TYPE/GPlasdi_venv/bin/activate
 source /collab/usr/gapps/python/toss_4_x86_64_ib/anaconda3-2023.03/bin/activate
 conda activate GPLaSDI
 
-./generate_data_sim.py;
+./generate_data_test.py;
