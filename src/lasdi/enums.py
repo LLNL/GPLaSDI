cc1efc2fb1ec4d045f820e1eab844131f571780a
Robert Stephany <stephany1@llnl.gov>
2024-10-24 07:39:28 -0700

    Added documentation to inputs.py
    I also reformatted enums.py

--- src/lasdi/enums.py
+++ src/lasdi/enums.py
@@ -1,13 +1,13 @@
 from enum import Enum
 
 class NextStep(Enum):
-    Train = 1
-    PickSample = 2
-    RunSample = 3
-    CollectSample = 4
+    Train           = 1
+    PickSample      = 2
+    RunSample       = 3
+    CollectSample   = 4
 
 class Result(Enum):
-    Unexecuted = 1
-    Success = 2
-    Fail = 3
-    Complete = 4
+    Unexecuted      = 1
+    Success         = 2
+    Fail            = 3
+    Complete        = 4
