3e71728eac819f336f221177a36e418201f756cd
mtrautner <margarettrautner@gmail.com>
2025-08-19 11:17:42 -0700

    Fixed array checkpoint issue: each job needs its own checkpoint directory

--- examples/pareto-experiments/make-ymls.py
+++ examples/pareto-experiments/make-ymls.py
@@ -1,5 +1,5 @@
 import yaml
-
+import os
 template_file = 'burgers1d-nonadaptive.yml'
 
 with open(template_file, 'r') as file:
@@ -12,10 +12,15 @@ for max_iter in max_iters:
     for hidden_unit in hidden_units:
         new_template = template.copy()
         new_template['lasdi']['gplasdi']['max_iter'] = max_iter
+        new_template['lasdi']['gplasdi']['n_iter'] = max_iter
         new_template['latent_space']['ae']['hidden_units'] = [hidden_unit]
         exp_key = f"burgers1d-NA-MI{max_iter}-HU{hidden_unit}"
         new_template['exp_key'] = exp_key
-        filename = f"burgers1d-NA-MI{max_iter}-HU{hidden_unit}.yml"
-        with open(filename, 'w') as outfile:
+        new_template['lasdi']['gplasdi']['path_checkpoint'] = f"{exp_key}/checkpoint"
+        new_template['lasdi']['gplasdi']['results'] = f"{exp_key}"
+        folder_name = exp_key
+        # Create folder if it does not exist
+        os.makedirs(folder_name, exist_ok=True)
+        os.makedirs(folder_name + '/checkpoint/', exist_ok=True)
+        with open(folder_name + '/config.yaml', 'w') as outfile:
             yaml.dump(new_template, outfile, default_flow_style=False)
-        print(f"Created: {filename}")
